98c29b6004a098d8161090213556c5678899aeb2
Alexis Villegas Torres <alexisvt@outlook.com>
2017-11-16 16:00:28 -0600

    Adding relations and automigrate

--- getting-started/server/boot/auto-update.js
+++ getting-started/server/boot/auto-update.js
@@ -5,6 +5,20 @@ module.exports = function(app) {
     if (err) {
       throw err;
     }
-    console.log('Model Synced');
+    console.log('Product table Synced');
+  });
+
+  app.dataSources.MySQL.autoupdate('Company', err => {
+    if (err) {
+      throw err;
+    }
+    console.log('Company table Synced');
+  });
+
+  app.dataSources.MySQL.autoupdate('Account', err => {
+    if (err) {
+      throw err;
+    }
+    console.log('Account table Synced');
   });
 };
